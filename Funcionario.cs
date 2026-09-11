cf2d69976765fcf408ff9c5fb2c0ff821eaeb0ca
Mathjoa989 <matheus.f.silva18@edu.senai.br>
2026-09-11 11:02:11 -0300

    Atividade2

---
+++ Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+
+        public virtual double CalcularBonusAnual()
+        {
+            return Salario * 0.10;
+        }
+
+    }
+    public class Gerente : Funcionario
+    {
+        public new double CalcularBonusAnual()
+        {
+            return (Salario * 0.15) + 1000.0;
+        }
+    }
+
+    public class Desenvolvedor : Funcionario
+    {
+        public new double CalcularBonusAnual()
+        {
+            return Salario * 0.12;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
-﻿namespace Heranca
+﻿using Heranca;
+
+namespace Heranca
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Cachorro meuCachorro = new Cachorro("Rex", "Labrador");
-            meuCachorro.Comer();
-            meuCachorro.Latir();
+            Gerente gerente = new Gerente();
+            gerente.Nome = "Carlos";
+            gerente.Salario = 8000.0;
 
-            Gato meuGato = new Gato("Davi", "Siamês");
-            meuGato.Comer();
-            meuGato.Miado();
+            Desenvolvedor dev = new Desenvolvedor();
+            dev.Nome = "Cesar";
+            dev.Salario = 5000.0;
+
+            Console.WriteLine($"Gerente: {gerente.Nome}");
+            Console.WriteLine($"Salário: R$ {gerente.Salario:F2}");
+            Console.WriteLine($"Bônus Anual: R$ {gerente.CalcularBonusAnual():F2}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Desenvolvedor: {dev.Nome}");
+            Console.WriteLine($"Salário: R$ {dev.Salario:F2}");
+            Console.WriteLine($"Bônus Anual: R$ {dev.CalcularBonusAnual():F2}");
+
+
         }
     }
 }
