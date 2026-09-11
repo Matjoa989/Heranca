a21201bba41da4683a3c6c497d26155a492fb9a1
Mathjoa989 <matheus.f.silva18@edu.senai.br>
2026-09-11 10:53:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro meuCachorro = new Cachorro("Rex", "Labrador");
+            meuCachorro.Comer();
+            meuCachorro.Latir();
+
+            Gato meuGato = new Gato("Davi", "Siamês");
+            meuGato.Comer();
+            meuGato.Miado();
+        }
+    }
+}
